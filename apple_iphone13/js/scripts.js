bef182daa0d1457f3f6ab1fdd29ea3e7ca312c75
SIC-nicolastafle <nicolas.tafle@smartimprove.pt>
2026-03-18 00:26:53 +0000

    [ADD] apple_iphone13

---
+++ apple_iphone13/js/scripts.js
@@ -0,0 +1,27 @@
+// Elements
+const buttons = document.querySelectorAll('#image-picker li');
+const image = document.querySelector('#product-image');
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        console.log(e);
+
+        // remove a class selected a todos os botões antes de definir
+        buttons.forEach((btn) => {
+            btn.querySelector('.color').classList.remove('selected');
+        });
+
+        const button = e.target;
+
+        const id = button.getAttribute('id');
+
+        button.querySelector('.color').classList.add('selected');
+
+        image.classList.add('changing');
+        image.setAttribute('src', `img/iphone_${id}.jpg`);
+
+        setTimeout(() => {
+            image.classList.toggle('changing')
+        },200)
+    })
+})
